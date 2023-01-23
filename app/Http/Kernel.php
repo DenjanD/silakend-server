a72624866abedfdbb779393b52b6ceb67b32a92b
DenjanD <alfien311@gmail.com>
2023-01-23 20:17:38 +0700

    Hotfix: CORS fix--system

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,8 +22,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ForceJsonResponse::class,
-        \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
+        // \Fruitcake\Cors\HandleCors::class,
+        // \App\Http\Middleware\Cors::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
